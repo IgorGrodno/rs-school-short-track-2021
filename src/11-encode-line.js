7e962c6736a00f76801be0c98c48ac31deb4d91e
IgorGrodno <5850462@mail.ru>
2021-04-11 19:09:22 +0300

    feat: encode line task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  const arr = str.split('');
+  let tmp = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      tmp++;
+    } else {
+      if (tmp > 1) {
+        result += tmp;
+        tmp = 1;
+      }
+      result += arr[i];
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
