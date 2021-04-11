b4843bcd96fab2b16c3ee74a8319b994a0892a8c
IgorGrodno <5850462@mail.ru>
2021-04-11 20:55:15 +0300

    feat: stack task

--- test/13-stack-tests.js
+++ test/13-stack-tests.js
@@ -3,13 +3,13 @@ const Stack = require('../src/13-stack');
 it.optional = require('../extensions/it-optional');
 
 describe('13-stack', () => {
-  it.optional('should create a Stack with the methods', () => {
-    const stack = new Stack();
-    assert.doesNotThrow(() => stack.push(5));
-    assert.doesNotThrow(() => stack.push(6));
-    assert.doesNotThrow(() => stack.push(7));
-    assert.strictEqual(stack.peek(), 7);
-    assert.strictEqual(stack.pop(), 7);
-    assert.strictEqual(stack.peek(), 6);
-  });
+    it.optional('should create a Stack with the methods', () => {
+        const stack = new Stack();
+        assert.doesNotThrow(() => stack.push(5));
+        assert.doesNotThrow(() => stack.push(6));
+        assert.doesNotThrow(() => stack.push(7));
+        assert.strictEqual(stack.peek(), 7);
+        assert.strictEqual(stack.pop(), 7);
+        assert.strictEqual(stack.peek(), 6);
+    });
 });
